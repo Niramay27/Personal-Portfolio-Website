44d3ce8a88cc7ca89181d145cf7d984d53d024de
Niramay27 <142372471+Niramay27@users.noreply.github.com>
2024-07-20 12:42:08 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -75,7 +75,7 @@ let detailsArray = [
     "Used Decision Tree Classifier algorithm to detect Hate Speech on Twitter data", "Used Naive Bayes model to classify mails as Spam or Ham", "Efficiently used Data Exploration Skills to optimize risk assessment and factors influencing creditworthiness", "Flask application designed to allocate rooms to groups based on their uploaded CSV files containing group and hostel data.", "Creatively designed this very own portfolio website using HTML,CSS,JS", "A web news application that uses api to fetch news and displays it categorically", "A beginner project of responsive music player with limited functionalities"
 ];
 let linkArray = [
-    "https://github.com/Niramay27/Basic-ML-projects", "https://github.com/Niramay27/Basic-ML-projects", "https://github.com/Niramay27/Basic-ML-projects", "https://github.com/Niramay27/Digitalization-of-the-Hospitality-Process", "https://www.google.com/", "https://github.com/Niramay27/news_app", "https://github.com/Niramay27/music-player/tree/main"
+    "https://github.com/Niramay27/Basic-ML-projects", "https://github.com/Niramay27/Basic-ML-projects", "https://github.com/Niramay27/Basic-ML-projects", "https://github.com/Niramay27/Digitalization-of-the-Hospitality-Process", "https://portfolio-y344.onrender.com/", "https://github.com/Niramay27/news_app", "https://github.com/Niramay27/music-player/tree/main"
 ];
 
 let certi_name = [
